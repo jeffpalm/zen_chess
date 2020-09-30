68d1c58ed185c2ab223d0d2dd5c590e58ef98af6
Jeff Palmer <jeff@palmytree.com>
2020-09-29 19:35:52 -0500

    Safari AudioContext compatibility

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,79 +1,80 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { AudioContext } from 'standardized-audio-context';
 
 export default class Music extends Component {
 	constructor() {
-		super()
+		super();
 		this.state = {
-			audioCtx: new AudioContext()
-		}
-		this.state.gainNode = this.state.audioCtx.createGain()
-		this.state.filterNode = this.state.audioCtx.createBiquadFilter()
-		this.state.analyserNode = this.state.audioCtx.createAnalyser()
-		this.state.filterNode.type = 'lowpass'
-		this.state.gainNode.gain.value = 0.1
+			audioCtx: new AudioContext(),
+		};
+		this.state.gainNode = this.state.audioCtx.createGain();
+		this.state.filterNode = this.state.audioCtx.createBiquadFilter();
+		this.state.analyserNode = this.state.audioCtx.createAnalyser();
+		this.state.filterNode.type = 'lowpass';
+		this.state.gainNode.gain.value = 0.1;
 	}
 
 	componentDidUpdate() {
-		const { filterNode, audioCtx, audioPlayer, analyserNode } = this.state
+		const { filterNode, audioCtx, audioPlayer, analyserNode } = this.state;
 
 		if (audioCtx.state === 'suspended') {
-			audioCtx.resume()
+			audioCtx.resume();
 		}
 
 		if (this.props.preMove) {
 			filterNode.frequency.exponentialRampToValueAtTime(
 				200,
 				audioCtx.currentTime + 0.5
-			)
+			);
 		} else if (!this.props.preMove) {
 			filterNode.frequency.exponentialRampToValueAtTime(
 				5000,
 				audioCtx.currentTime + 0.2
-			)
+			);
 		}
 
 		if (this.props.musicPlay) {
-			audioPlayer.play()
+			audioPlayer.play();
 		} else if (!this.props.musicPlay) {
-			audioPlayer.pause()
+			audioPlayer.pause();
 		}
 
 		if (this.props.soundOn) {
-			audioPlayer.muted = false
+			audioPlayer.muted = false;
 		} else if (!this.props.soundOn) {
-			audioPlayer.muted = true
+			audioPlayer.muted = true;
 		}
 		setInterval(() => {
-			let freqArray = new Float32Array(analyserNode.frequencyBinCount)
-			const chessBoard = document.querySelector('#chess-board')
+			let freqArray = new Float32Array(analyserNode.frequencyBinCount);
+			const chessBoard = document.querySelector('#chess-board');
 			if (this.props.musicPlay && chessBoard) {
-				void analyserNode.getFloatFrequencyData(freqArray)
+				void analyserNode.getFloatFrequencyData(freqArray);
 				// console.log(freqArray)
-				let output = freqArray.reduce((acc, cur) => -1 / (acc + cur), 0)
-				output *= 10000
-				output -= 40
-				output = Math.floor(output * 5)
+				let output = freqArray.reduce((acc, cur) => -1 / (acc + cur), 0);
+				output *= 10000;
+				output -= 40;
+				output = Math.floor(output * 5);
 				// console.log(output)
-				chessBoard.style.boxShadow = `0px 0px ${output}px 0px white`
+				chessBoard.style.boxShadow = `0px 0px ${output}px 0px white`;
 			} else if (!this.props.musicPlay && chessBoard) {
-				chessBoard.style.boxShadow = `0px 0px 10px 0px white`
+				chessBoard.style.boxShadow = `0px 0px 10px 0px white`;
 			}
-		}, 17)
+		}, 17);
 	}
 	componentDidMount() {
-		const audioPlayer = document.querySelector('#audio-player')
+		const audioPlayer = document.querySelector('#audio-player');
 
-		audioPlayer.loop = true
-		audioPlayer.currentTime = Math.ceil(Math.random() * 3000)
+		audioPlayer.loop = true;
+		audioPlayer.currentTime = Math.ceil(Math.random() * 3000);
 		// console.log(audioPlayer.prototype)
-		this.setState({ audioPlayer })
-		const { audioCtx, gainNode, filterNode, analyserNode } = this.state
-		const track = audioCtx.createMediaElementSource(audioPlayer)
+		this.setState({ audioPlayer });
+		const { audioCtx, gainNode, filterNode, analyserNode } = this.state;
+		const track = audioCtx.createMediaElementSource(audioPlayer);
 		track
 			.connect(gainNode)
 			.connect(filterNode)
 			.connect(analyserNode)
-			.connect(audioCtx.destination)
+			.connect(audioCtx.destination);
 	}
 	render() {
 		return (
@@ -81,8 +82,9 @@ export default class Music extends Component {
 				<audio
 					id='audio-player'
 					src='assets/music/Lo-fi-beats.ogg'
-					autoPlay></audio>
+					autoPlay
+				></audio>
 			</div>
-		)
+		);
 	}
 }
